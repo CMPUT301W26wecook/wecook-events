bfa022cd30d595f7d56927648106ab90a02459ad
Willy Zuo <15186062353@163.com>
2026-03-15 02:41:53 -0600

    Add smoke-fail androidTest assertion to verify PR blocking

--- app/src/androidTest/java/com/example/wecookproject/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/wecookproject/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.wecookproject", appContext.getPackageName());
+        assertEquals("com.example.wecookproject", appContext.getPackageName());`r`n        assertEquals("Intentional smoke failure", 1, 2);
     }
 }
